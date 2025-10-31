b4652a883e328ad8709bbef75796689a40b45acd
Aleena Baby <aleenababy68@gmail.com>
2025-10-31 18:43:02 +0530

    Netflix

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+
+import React from 'react';
+import NavBar from './Components/NavBar/NavBar';
+import './App.css'
+import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
+import {action,originals} from './urls'
+
+function App() {
+  return (
+    <div>
+    <NavBar/>
+    <Banner/>
+    <RowPost url={originals} title='Netflix Originals'/>
+    <RowPost url={action} title='Action' isSmall={true}/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants/constants.js
@@ -0,0 +1,5 @@
+
+
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const  API_KEY = 'e85c0e8bd37db56d07d604d8d2a74af7';
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
